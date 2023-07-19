1b091ddfc72c6778c6a1b387da6e438806c2e8ab
cris <cridonoso@inf.udec.cl>
2023-07-18 21:14:40 -0400

    mcmc

--- core/mcmc.py
+++ core/mcmc.py
@@ -19,7 +19,7 @@ def likelihood_fn(flux, cube, psf, rot_ang, back_med, back_std):
 	residuals_std = tf.math.reduce_std(residuals)
 	residuals_med = tfp.stats.percentile(residuals, q=50)
 
-	loss_std = tf.pow(back_std - residuals_std, 2)
+	loss_std = tf.abs(back_std - residuals_std)
 	# loss_med = tf.pow(back_med - residuals_med, 2)
 	return loss_std
 
